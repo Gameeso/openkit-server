8f860da2c75d4c68f48ec1f9963000d93dce4771
Peter Wilemsen <peter@codebuffet.co>
2014-06-15 23:22:27 +0200

    Finally starting to get OpenKit-Server to install after 2 days :(

--- cloud_images/scripts/bundle_install_openkit_server.sh
+++ cloud_images/scripts/bundle_install_openkit_server.sh
@@ -1,2 +1,7 @@
-cd /vagrant/openkit-server/dashboard
-bundle install
+cd /vagrant
+git clone -b development https://github.com/Gameeso/openkit-server.git
+cd openkit-server/dashboard
+
+bundle update --path vendor/bundle
+bundle exec bin/rake db:create RAILS_ENV=development
+start gameeso
--- cloud_images/scripts/install_openkit_server.sh
+++ cloud_images/scripts/install_openkit_server.sh
@@ -7,14 +7,19 @@ apt-get update
 echo mysql-server mysql-server/root_password password gameeso | sudo debconf-set-selections
 echo mysql-server mysql-server/root_password_again password gameeso | sudo debconf-set-selections
 
-apt-get install -y mysql-server ruby2.1 ruby2.1-dev mysql-client libmysqlclient-dev libsqlite3-dev libxslt1-dev redis-server
+apt-get install -y git mysql-server ruby2.1 ruby2.1-dev mysql-client libmysqlclient-dev libsqlite3-dev libxslt1-dev redis-server build-essential
 
 gem install bundle
-apt-get install -y git
 
 mkdir -p /vagrant
-chmod 744 -R /vagrant
-cd /vagrant
+chmod 7777 -R /vagrant
 
-git clone -b development https://github.com/Gameeso/openkit-server.git
-cd openkit-server/dashboard
+cat >/etc/init/gameeso.conf <<EOL
+description "Gameeso Game Backend"
+ 
+start on filesystem or runlevel [2345]
+stop on run level [!2345]
+ 
+exec start-stop-daemon --start --chuid vagrant --chdir /vagrant/openkit-server/dashboard/ --exec "/vagrant/openkit-server/dashboard/bin/rails" \
+    -- server
+EOL
