53a16ac4b9438ab43d8e777a4958c465ae0f85eb
Peter Wilemsen <peter@codebuffet.co>
2014-06-16 00:14:03 +0200

    Added support for exporting images to digitalocean!

--- cloud_images/scripts/bundle_install_openkit_server.sh
+++ cloud_images/scripts/bundle_install_openkit_server.sh
@@ -2,6 +2,8 @@ cd /vagrant
 git clone -b development https://github.com/Gameeso/openkit-server.git
 cd openkit-server/dashboard
 
-bundle update --path vendor/bundle
+bundle install --path vendor/bundle
+bundle update
 bundle exec bin/rake db:create RAILS_ENV=development
+
 start gameeso
