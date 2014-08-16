382600f919350e2585b9d18e2c04783dc19d94ad
Peter Wilemsen <peter@codebuffet.co>
2014-08-16 23:30:03 +0200

    Changed cloud_images to install NodeJS and CoffeeScript for the importer

--- cloud_images/scripts/install_openkit_server.sh
+++ cloud_images/scripts/install_openkit_server.sh
@@ -2,13 +2,19 @@ echo "gem: --no-rdoc --no-ri" > ~/.gemrc
 
 apt-get install -y python-software-properties software-properties-common
 apt-add-repository -y ppa:brightbox/ruby-ng
+add-apt-repository -y ppa:chris-lea/node.js
+
 apt-get update
 
 echo mysql-server mysql-server/root_password password gameeso | sudo debconf-set-selections
 echo mysql-server mysql-server/root_password_again password gameeso | sudo debconf-set-selections
 
-apt-get install -y git mysql-server ruby2.1 ruby2.1-dev mysql-client libmysqlclient-dev libsqlite3-dev libxslt1-dev redis-server build-essential
+apt-get install -y git nodejs npm mysql-server ruby2.1 ruby2.1-dev mysql-client libmysqlclient-dev libsqlite3-dev libxslt1-dev redis-server build-essential
 
+ln -s /usr/bin/nodejs /usr/bin/node
+
+# CoffeeScript transpiler for OpenKit-Importer
+npm install -g coffee-script
 gem install bundle
 
 # Firewall
