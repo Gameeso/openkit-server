f8cd6d82c38ed6d2a298bf544769f5eafa3ddeb7
Peter Wilemsen <peter@codebuffet.co>
2014-08-29 19:47:26 +0200

    Update ok_config.rb to have a more consistent email-configuration

--- dashboard/config/initializers/setup_mail.rb
+++ dashboard/config/initializers/setup_mail.rb
@@ -1,13 +1,13 @@
 ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
+  :address              => OKConfig[:mail_address],
+  :port                 => OKConfig[:mail_port],
   :domain               => OKConfig[:mail_domain],
-  :user_name            => OKConfig[:mail_user],
-  :password             => OKConfig[:mail_pass],
-  :authentication       => "plain",
-  :enable_starttls_auto => true
+  :user_name            => OKConfig[:mail_user_name],
+  :password             => OKConfig[:mail_password],
+  :authentication       => OKConfig[:mail_authentication],
+  :enable_starttls_auto => OKConfig[:mail_enable_starttls_auto]
 }
 
-mailer_host = OKConfig[:mailer_host]
+mailer_host = OKConfig[:mail_host]
 ActionMailer::Base.default_url_options[:host] = mailer_host
 # Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
